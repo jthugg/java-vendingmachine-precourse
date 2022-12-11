0666da562e926921010eff50e98daee68fb04e57
jthugg <jthug@naver.com>
2022-12-11 14:22:36 +0900

    Feat: 자판기가 가지고 있을 동전 입력 요청 메세지 출력 기능 추가

--- src/main/java/vendingmachine/controllers/IOController.java
+++ src/main/java/vendingmachine/controllers/IOController.java
@@ -1,5 +1,10 @@
 package vendingmachine.controllers;
 
+import vendingmachine.utils.Ui;
+
 public class IOController {
 
+	public static void printRequestInitMoney() {
+		Ui.printRequestInitMoney();
+	}
 }
--- src/main/java/vendingmachine/utils/Ui.java
+++ src/main/java/vendingmachine/utils/Ui.java
@@ -1,5 +1,22 @@
 package vendingmachine.utils;
 
-public class Ui {
+public enum Ui {
 
+	INIT_MONEY("자판기가 보유하고 있는 금액을 입력해 주세요.");
+
+	private String value;
+
+	Ui(String value) {
+		this.value = value;
+	}
+
+	private int number;
+
+	Ui(int number) {
+		this.number = number;
+	}
+
+	public static void printRequestInitMoney() {
+		System.out.println(INIT_MONEY.value);
+	}
 }
